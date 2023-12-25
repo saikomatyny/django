1d1ce38ca670c2984ac2891613fabbc7abe27540
saikomatyny <1020vleydgod1020@gmail.com>
2023-12-25 21:25:02 +0200

    Auto commit at Пн 25 дек 2023 21:25:02 EET

--- todolist/models.py
+++ todolist/models.py
@@ -6,7 +6,7 @@ class todolists(models.Model):
 
 
     def __str__(self):
-        return {self.name_of_task: self.done_or_not}
+        return self.name_of_task
     
     class Meta:
         verbose_name_plural, verbose_name = 'Список дел', 'Список дел'
