8c358a4e477d21d8a9a3dd5be9dd9ef820b7b440
saikomatyny <1020vleydgod1020@gmail.com>
2023-12-25 21:40:37 +0200

    Auto commit at Пн 25 дек 2023 21:40:37 EET

---
+++ todolist/views.py
@@ -0,0 +1,48 @@
+import json
+from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from .models import todolists
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+
+def get_endpoint(request):
+    todolist_objects = todolists.objects.all()
+    todolist_values_dict = {}
+
+    for obj in todolist_objects:
+        todolist_values_dict[obj.name_of_task] = obj.done_or_not
+
+    return JsonResponse(todolist_values_dict)
+
+@api_view(['POST'])
+def post_endpoint(request):
+    data = request.data  # Предполагается, что данные приходят в виде словаря JSON
+
+    try:
+        name_of_task = data['name_of_task']
+        done_or_not = data['done_or_not']
+
+        todolist_obj = todolists.objects.create(name_of_task=name_of_task, done_or_not=done_or_not)
+        
+        response_data = {
+            'id': todolist_obj.id,
+            'name_of_task': todolist_obj.name_of_task,
+            'done_or_not': todolist_obj.done_or_not,
+        }
+
+        return Response(response_data, status=status.HTTP_201_CREATED)
+
+    except KeyError as e:
+        error_message = f'Missing key in data: {str(e)}'
+        return Response({'error': error_message}, status=status.HTTP_400_BAD_REQUEST)
+
+
+def index(request):
+    data = {
+        'title': 'example #1',
+        'main_text' : 'test test test',
+        'list' : [1, 2, 3, '4', '5']
+    }
+    return render(request, 'main1/index.html', data)
