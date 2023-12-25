2077989d04d1fadb790fbdc1c48f740b055fd8d7
saikomatyny <1020vleydgod1020@gmail.com>
2023-12-25 17:36:33 +0200

    Auto commit at Пн 25 дек 2023 17:36:33 EET

--- todolist/urls.py
+++ todolist/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index)
+    path('', views.index),
+    path('get_endpoint/', views.get_endpoint),
+    path('post_endpoint/', views.post_endpoint)
 ]
