6f6ce0a493b3295de0e2d2018127985f5f99bff7
saikomatyny <1020vleydgod1020@gmail.com>
2023-12-25 20:53:16 +0200

    Auto commit at Пн 25 дек 2023 20:53:16 EET

--- todolist/models.py
+++ todolist/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class todolists(models.Model):
     name_of_task = models.CharField('Что нужно сделать: ', max_length = 100)
-    if_complete = models.BooleanField('Сделано или нет')
+    done_or_not = models.BooleanField('Сделано или нет')
 
 
     def __str__(self):
