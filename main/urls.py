ab46847e4ef8bde1bb1aa5986018595b096455ef
saikomatyny <1020vleydgod1020@gmail.com>
2023-12-25 17:28:04 +0200

    Auto commit at Пн 25 дек 2023 17:28:04 EET

--- main/urls.py
+++ main/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('main1.urls'))
+    path('', include('main1.urls')),
+    path('', include('todolist.urls'))
 ]
