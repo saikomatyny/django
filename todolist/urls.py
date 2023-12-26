2ed1fa77d03d9e85880e75afe1eead94c2813674
saikomatyny <1020vleydgod1020@gmail.com>
2023-12-26 14:24:02 +0200

    Auto commit at Вт 26 дек 2023 14:24:02 EET

--- todolist/urls.py
+++ todolist/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.index),
     path('get_endpoint/', views.get_endpoint),
-    path('post_endpoint/', views.post_endpoint)
+    path('post_endpoint/', views.post_endpoint),
+    path('delete_endpoint/', views.delete_endpoint)
 ]
