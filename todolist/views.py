a0046a0a08e8e137cf94c41c76c1b37e1c65e4b1
saikomatyny <1020vleydgod1020@gmail.com>
2023-12-26 19:18:58 +0200

    Auto commit at Вт 26 дек 2023 19:18:58 EET

--- todolist/views.py
+++ todolist/views.py
@@ -43,18 +43,16 @@ def deleteToDoList(request):
     data = request.data
 
     try:
-        if isinstance(data, list):
+        if type(data) == list:
             for name in data:
                 try:
-                    todolist = todolists.objects.get(name_of_task=name)
-                    todolist.delete()
+                    todolists.objects.get(name_of_task=name).delete()
                 except todolists.DoesNotExist:
                     error_message = f'There is no such task as {name}'
                     return Response({'error': error_message}, status=status.HTTP_400_BAD_REQUEST)
         else:
             try:
-                todolist = todolists.objects.get(name_of_task=data)
-                todolist.delete()
+                todolists.objects.get(name_of_task=data).delete()
             except todolists.DoesNotExist:
                 error_message = f'There is no such task as {data}'
                 return Response({'error': error_message}, status=status.HTTP_400_BAD_REQUEST)
